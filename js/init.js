7bb30b85e3f01794a8d4b187ecbb8f054d8d5dce
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-15 09:10:16 +0200

    first commit

---
+++ js/init.js
@@ -0,0 +1,22 @@
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
+
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is opened
+      onClose: function(el) { /* Do Stuff */ }, // A function to be called when sideNav is closed
+    });
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
